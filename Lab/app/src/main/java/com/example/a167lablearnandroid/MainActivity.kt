0c53eb7832da3e337aad28784cbbac4d51df34e1
PhitchakornJalas <phitchakorn16351pp@gmail.com>
2026-01-27 16:34:20 +0700

    upload RPG Character Card

--- Lab/app/src/main/java/com/example/a167lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/a167lablearnandroid/MainActivity.kt
@@ -1,47 +1,69 @@
 package com.example.a167lablearnandroid
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a167lablearnandroid.ui.theme._167LabLearnAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
-            _167LabLearnAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Gray)
+                .padding(32.dp)) {
+
+                // hp
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(32.dp)
+                    .background(Color.White)) {
+                    Text(
+                        text = "hp",
+                        color = Color.White,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterStart)
+                            .fillMaxWidth(fraction = 0.67f)
+                            .background(Color.Red)
+                            .padding(8.dp)
                     )
                 }
+
+                // image
+                val image = painterResource(R.drawable.wei)
+                Box() {
+                    Image(
+                        painter = image,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .align(alignment = Alignment.TopCenter)
+                    )
+                }
+
+                // status
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _167LabLearnAndroidTheme {
-        Greeting("Android")
     }
 }
