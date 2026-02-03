dc2b2bdbe538e9c9c7419c3b7ea736c702a256ae
PhitchakornJalas <phitchakorn16351pp@gmail.com>
2026-02-03 23:38:45 +0700

    complete beautiful RGP card

--- Lab/app/src/main/java/com/example/a167lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/a167lablearnandroid/MainActivity.kt
@@ -8,17 +8,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,9 +37,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,7 +55,7 @@ class MainActivity : ComponentActivity() {
 //        enableEdgeToEdge()
         setContent {
             RPGCardView()
-
+            previewScreen()
         }
     }
 
@@ -49,14 +63,18 @@ class MainActivity : ComponentActivity() {
     fun RPGCardView() {
         Column(modifier = Modifier
             .fillMaxSize()
-            .background(Color.Gray)
-            .padding(32.dp)) {
+            .background(Color.White)
+            .padding(32.dp),
+            verticalArrangement = Arrangement.Center) {
 
             // hp
             Box(modifier = Modifier
                 .fillMaxWidth()
                 .height(32.dp)
-                .background(Color.White)) {
+                .background(Color.White)
+                .border(
+                    width = 2.dp, color = Color.Black)
+            ) {
                 Text(
                     text = "hp",
                     color = Color.White,
@@ -74,55 +92,156 @@ class MainActivity : ComponentActivity() {
                 contentDescription = "Profile",
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-                    .padding(top = 16.dp, bottom = 32.dp)
+                    .padding(top = 64.dp, bottom = 64.dp)
+                    .clip(RoundedCornerShape(16.dp)
+                )
                     .clickable {
                         startActivity((Intent(this@MainActivity, ListActivity::class.java)))
                     }
             )
 
-            var status1 by remember { mutableStateOf(99) }
-            var status2 by remember { mutableStateOf(99) }
+            var status1 by remember { mutableStateOf(1) }
+            var status2 by remember { mutableStateOf(1) }
             var status3 by remember { mutableStateOf(99) }
             // status
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Column {
-                    Button(onClick = {
-                        status1++
-                    }) {
-//                    Image(
-//                        painter = painterResource(id = R.drawable.baseline_thumb_up_24)
-//                        contentDescription = "Profile"
-//                    )
-                    }
-                    Text(text = "หล่อ", fontSize = 32.sp)
+                Column(
+                    modifier = Modifier
+                        .width(100.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "หล่อ", fontSize = 32.sp,
+                        modifier = Modifier
+                    )
                     Text(text = status1.toString(), fontSize = 32.sp)
-                    Text(text = "-", fontSize = 32.sp,
-                        modifier = Modifier.clickable {
-                            status1--
-                        })
+                    Row {
+                        Button(onClick = { status1++ },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent
+                            ),
+                            contentPadding = PaddingValues(0.dp),
+                            modifier = Modifier
+                                .width(50.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.round_thumb_up_24),
+                                contentDescription = "thumbUp",
+                                modifier = Modifier
+                                    .size(32.dp)
+                            )
+                        }
+
+                        Button(onClick = { status1-- },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent
+                            ),
+                            contentPadding = PaddingValues(0.dp),
+                            modifier = Modifier
+                                .width(50.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.round_thumb_down_24),
+                                contentDescription = "thumbDown",
+                                modifier = Modifier
+                                    .size(32.dp)
+                            )
+                        }
+                    }
                 }
-                Column {
-                    Text(text = "+", fontSize = 32.sp)
+                Column(
+                    modifier = Modifier
+                        .width(100.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Text(text = "น่ารัก", fontSize = 32.sp)
                     Text(text = status2.toString(), fontSize = 32.sp)
-                    Text(text = "-", fontSize = 32.sp)
+                    Row {
+                        Button(onClick = { status2++ },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent
+                            ),
+                            contentPadding = PaddingValues(0.dp),
+                            modifier = Modifier
+                                .width(50.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.round_thumb_up_24),
+                                contentDescription = "thumbUp",
+                            )
+                        }
+
+                        Button(onClick = { status2-- },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent
+                            ),
+                            contentPadding = PaddingValues(0.dp),
+                            modifier = Modifier
+                                .width(50.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.round_thumb_down_24),
+                                contentDescription = "thumbDown",
+                                modifier = Modifier
+                                    .size(32.dp)
+                            )
+                        }
+                    }
                 }
-                Column {
-                    Text(text = "+", fontSize = 32.sp)
-                    Text(text = "ขี้เล่น", fontSize = 32.sp)
+                Column(
+                    modifier = Modifier
+                        .width(100.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "เต้น", fontSize = 32.sp)
                     Text(text = status3.toString(), fontSize = 32.sp)
-                    Text(text = "-", fontSize = 32.sp)
+                    Row {
+                        Button(onClick = { status3++ },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent
+                            ),
+                            contentPadding = PaddingValues(0.dp),
+                            modifier = Modifier
+                                .width(50.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.round_thumb_up_24),
+                                contentDescription = "thumbUp",
+                                modifier = Modifier
+                                    .size(32.dp)
+                            )
+                        }
+
+                        Button(onClick = { status3-- },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent
+                            ),
+                            contentPadding = PaddingValues(0.dp),
+                            modifier = Modifier
+                                .width(50.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.round_thumb_down_24),
+                                contentDescription = "thumbDown",
+                                modifier = Modifier
+                                    .size(32.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-//@Preview
-//@Composable
-//fun previewScreen() {
-//    RPGCardView()
-//}
+    @Preview
+    @Composable
+    fun previewScreen() {
+        RPGCardView()
+    }
+
+}
